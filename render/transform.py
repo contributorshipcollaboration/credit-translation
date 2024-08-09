bc50c7631f7e0db2494aeb1d4d74a2caacc2a205
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2024-08-09 11:22:37 +0200

    refactored transform.py to work with the new schema

--- render/transform.py
+++ render/transform.py
@@ -29,17 +29,13 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 translation_dir = os.path.join(script_dir, "../translation/")
 md_files_dir = os.path.join(script_dir, "../md_files/")
 
-def json_to_md(json_data, filename):
+def json_to_md(json_data, en_data, filename):
     """
     Convert JSON data to Markdown text following the specified structure.
     """
     # YAML header with title and date
     # Check if "languageName" exists in metadata, otherwise use get_language_name
-    if "languageName" in json_data["metadata"]:
-        language_name = json_data["metadata"]["languageName"]
-    else:
-        language_code = json_data["metadata"]["languageCode"]
-        language_name = get_language_name(language_code)
+    language_name = json_data["metadata"].get("languageName") or get_language_name(json_data["metadata"]["languageCode"])
     md_content = f"""---
 title: "{language_name} translation of CRediT"
 date: {datetime.now().strftime('%Y-%m-%d')}
@@ -51,19 +47,19 @@ githublink: "https://github.com/contributorshipcollaboration/credit-translation/
 """
 
     # Translations section as a table
-    md_content += "| English | " + language_name + " |\n"
+    md_content += f"| {language_name} | English |\n"
     md_content += "| --- | --- |\n"
 
-    for role, details in json_data["translations"].items():
-        role_name_en = "**" + role + "**"
-        role_name_translated = "**" + details["translation"]["name"] + "**"
-        description_en = details["description"]
-        description_translated = details["translation"]["description"]
+    for role_url, details in json_data["translations"].items():
+        role_name_en = f"**{en_data['translations'][role_url]['name']}**"
+        role_name_translated = f"**{details['name']}**"
+        description_en = en_data["translations"][role_url]["description"]
+        description_translated = details["description"]
         
         # Role name row
-        md_content += f"| {role_name_en} | {role_name_translated} |\n"
+        md_content += f"| {role_name_translated} | {role_name_en} |\n"
         # Description row
-        md_content += f"| {description_en} | {description_translated} |\n"
+        md_content += f"| {description_translated} | {description_en} |\n"
 
     # Translators section with ORCID hyperlink
     translators = json_data["metadata"]["translators"]
@@ -76,14 +72,16 @@ githublink: "https://github.com/contributorshipcollaboration/credit-translation/
     translator_names = ', '.join(translator_lines)
     md_content += f"\n## Translators\n\n{translator_names}\n\n"
 
+    # Link to JSON
+    md_content += f"## JSON Metadata\n\n[https://github.com/contributorshipcollaboration/credit-translation/blob/main/translations/{filename}]()\n\n"
     # License section 
-    md_content += f"\n## License\n\n[CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/)"
+    md_content += f"## License\n\n[CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/)"
     # copyright is held by the translators together with the originators of CRediT
-    md_content += f" {translator_names}, Brand, Allen, Altman, Hlava, & Scott"
+    md_content += f" {translator_names}, Brand, Allen, Altman, Hlava, & Scott\n\n"
     
     # Translation procedure section
     translation_procedure = json_data["metadata"]["translationProcedure"]
-    md_content += f"\n## Translation Procedure\n\n{translation_procedure}\n\n"
+    md_content += f"## Translation Procedure\n\n{translation_procedure}"
 
     return md_content
 
@@ -94,16 +92,20 @@ def transform_json_to_md(directory, output_directory):
     # Ensure output directory exists
     os.makedirs(output_directory, exist_ok=True)
 
+    # Load the English translation for reference
+    en_data = open_json_file(os.path.join(directory, "en.json"))
+
     for filename in os.listdir(directory):
-        if filename.startswith("credit_translation_") and filename.endswith(".json"):
+        if filename.endswith(".json") and filename != "en.json":
             json_path = os.path.join(directory, filename)
             try:
                 json_data = open_json_file(json_path)
             except ValueError as e:
                 print(f"Error processing {filename}: {e}")
+                continue
             
             # Convert JSON to Markdown
-            md_content = json_to_md(json_data, filename)
+            md_content = json_to_md(json_data, en_data, filename)
 
             # Save Markdown content to a new file
             md_filename = filename.replace(".json", ".md")
